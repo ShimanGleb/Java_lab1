676047ff7243ca27403790724174a2a554fc3e77
ShimanGleb <alien-g.shiman@yandex.ru>
2015-03-21 09:10:40 +0300

    Update Date.java

--- Date.java
+++ Date.java
@@ -6,6 +6,10 @@ public class Date {
 	int day=0;
 	String month;
 	int year=0;
+	int hour=0;
+	int minute=0;
+	int second=0;
+	int timeZone=0;
 	public void SplitDate()
 	{
 		int initialIndex=initial.indexOf('[')+1;
@@ -16,5 +20,11 @@ public class Date {
 		year+=(Integer.valueOf(initialIndex+9)-48)*100;
 		year+=(Integer.valueOf(initialIndex+10)-48)*10;
 		year+=Integer.valueOf(initialIndex+11)-48;
+		hour+=(Integer.valueOf(initialIndex+13)-48)*10;
+		hour+=Integer.valueOf(initialIndex+14)-48;
+		minute+=(Integer.valueOf(initialIndex+16)-48)*10;
+		minute+=Integer.valueOf(initialIndex+17)-48;
+		second+=(Integer.valueOf(initialIndex+19)-48)*10;
+		second+=Integer.valueOf(initialIndex+20)-48;
 	}
 }
